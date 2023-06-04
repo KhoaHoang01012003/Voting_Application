ba9f57dda1845fbfbd0a84cbf9577131e3eeb030
daovohuuhiep2003 <128503718+daovohuuhiep2003@users.noreply.github.com>
2023-06-04 18:59:49 +0700

    Info Candidate

---
+++ DOANMONHOC/DOANMONHOC/Info_Candidate.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace DOANMONHOC
+{
+    partial class Info_Candidate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            guna2PictureBox2 = new Guna.UI2.WinForms.Guna2PictureBox();
+            guna2PictureBox3 = new Guna.UI2.WinForms.Guna2PictureBox();
+            guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            guna2CircleButton1 = new Guna.UI2.WinForms.Guna2CircleButton();
+            guna2CirclePictureBox2 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            Label_name = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            Label_CampaignName = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            textBox1 = new TextBox();
+            guna2HtmlLabel4 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel5 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel6 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            Label_Slogan = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            Label_Name_Cadidate = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            Label_Class = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel9 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // guna2PictureBox1
+            // 
+            guna2PictureBox1.BorderRadius = 500;
+            guna2PictureBox1.CustomizableEdges = customizableEdges1;
+            guna2PictureBox1.FillColor = Color.FromArgb(133, 205, 253);
+            guna2PictureBox1.ImageRotate = 0F;
+            guna2PictureBox1.Location = new Point(-30, -5);
+            guna2PictureBox1.Name = "guna2PictureBox1";
+            guna2PictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2PictureBox1.Size = new Size(1440, 709);
+            guna2PictureBox1.TabIndex = 0;
+            guna2PictureBox1.TabStop = false;
+            // 
+            // guna2PictureBox2
+            // 
+            guna2PictureBox2.CustomizableEdges = customizableEdges3;
+            guna2PictureBox2.FillColor = Color.FromArgb(133, 205, 253);
+            guna2PictureBox2.ImageRotate = 0F;
+            guna2PictureBox2.Location = new Point(-30, -16);
+            guna2PictureBox2.Name = "guna2PictureBox2";
+            guna2PictureBox2.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2PictureBox2.Size = new Size(488, 376);
+            guna2PictureBox2.TabIndex = 1;
+            guna2PictureBox2.TabStop = false;
+            // 
+            // guna2PictureBox3
+            // 
+            guna2PictureBox3.CustomizableEdges = customizableEdges5;
+            guna2PictureBox3.FillColor = Color.FromArgb(133, 205, 253);
+            guna2PictureBox3.ImageRotate = 0F;
+            guna2PictureBox3.Location = new Point(590, -5);
+            guna2PictureBox3.Name = "guna2PictureBox3";
+            guna2PictureBox3.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            guna2PictureBox3.Size = new Size(850, 709);
+            guna2PictureBox3.TabIndex = 2;
+            guna2PictureBox3.TabStop = false;
+            // 
+            // guna2CirclePictureBox1
+            // 
+            guna2CirclePictureBox1.BackColor = Color.Transparent;
+            guna2CirclePictureBox1.FillColor = SystemColors.Window;
+            guna2CirclePictureBox1.Image = Properties.Resources.Hiepne;
+            guna2CirclePictureBox1.ImageFlip = Guna.UI2.WinForms.Enums.FlipOrientation.Horizontal;
+            guna2CirclePictureBox1.ImageRotate = 0F;
+            guna2CirclePictureBox1.Location = new Point(95, 471);
+            guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
+            guna2CirclePictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges7;
+            guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            guna2CirclePictureBox1.Size = new Size(250, 250);
+            guna2CirclePictureBox1.TabIndex = 3;
+            guna2CirclePictureBox1.TabStop = false;
+            guna2CirclePictureBox1.UseTransparentBackground = true;
+            // 
+            // guna2CircleButton1
+            // 
+            guna2CircleButton1.BackColor = Color.Transparent;
+            guna2CircleButton1.DisabledState.BorderColor = Color.DarkGray;
+            guna2CircleButton1.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2CircleButton1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2CircleButton1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2CircleButton1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            guna2CircleButton1.ForeColor = Color.White;
+            guna2CircleButton1.Location = new Point(131, 239);
+            guna2CircleButton1.Name = "guna2CircleButton1";
+            guna2CircleButton1.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            guna2CircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            guna2CircleButton1.Size = new Size(70, 70);
+            guna2CircleButton1.TabIndex = 4;
+            guna2CircleButton1.Text = "QUAY LẠI";
+            guna2CircleButton1.UseTransparentBackground = true;
+            // 
+            // guna2CirclePictureBox2
+            // 
+            guna2CirclePictureBox2.BackColor = Color.Transparent;
+            guna2CirclePictureBox2.BackgroundImageLayout = ImageLayout.Zoom;
+            guna2CirclePictureBox2.Image = Properties.Resources.logo_truong3;
+            guna2CirclePictureBox2.ImageRotate = 0F;
+            guna2CirclePictureBox2.Location = new Point(58, 12);
+            guna2CirclePictureBox2.Name = "guna2CirclePictureBox2";
+            guna2CirclePictureBox2.ShadowDecoration.CustomizableEdges = customizableEdges9;
+            guna2CirclePictureBox2.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            guna2CirclePictureBox2.Size = new Size(223, 209);
+            guna2CirclePictureBox2.TabIndex = 5;
+            guna2CirclePictureBox2.TabStop = false;
+            guna2CirclePictureBox2.UseTransparentBackground = true;
+            // 
+            // Label_name
+            // 
+            Label_name.BackColor = Color.FromArgb(133, 205, 253);
+            Label_name.Font = new Font("Segoe UI", 77F, FontStyle.Bold, GraphicsUnit.Pixel);
+            Label_name.ForeColor = Color.White;
+            Label_name.Location = new Point(590, 139);
+            Label_name.Name = "Label_name";
+            Label_name.Size = new Size(690, 104);
+            Label_name.TabIndex = 6;
+            Label_name.Text = "ĐÀO VÕ HỮU HIỆP";
+            // 
+            // guna2HtmlLabel1
+            // 
+            guna2HtmlLabel1.BackColor = Color.FromArgb(133, 205, 253);
+            guna2HtmlLabel1.Font = new Font("Segoe UI", 60F, FontStyle.Bold, GraphicsUnit.Pixel);
+            guna2HtmlLabel1.ForeColor = Color.White;
+            guna2HtmlLabel1.Location = new Point(443, 160);
+            guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            guna2HtmlLabel1.Size = new Size(126, 83);
+            guna2HtmlLabel1.TabIndex = 7;
+            guna2HtmlLabel1.Text = "BẦU";
+            // 
+            // guna2HtmlLabel2
+            // 
+            guna2HtmlLabel2.BackColor = Color.FromArgb(133, 205, 253);
+            guna2HtmlLabel2.Font = new Font("Segoe UI", 50F, FontStyle.Bold, GraphicsUnit.Pixel);
+            guna2HtmlLabel2.ForeColor = Color.White;
+            guna2HtmlLabel2.Location = new Point(798, 239);
+            guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            guna2HtmlLabel2.Size = new Size(110, 69);
+            guna2HtmlLabel2.TabIndex = 8;
+            guna2HtmlLabel2.Text = "CHO";
+            // 
+            // Label_CampaignName
+            // 
+            Label_CampaignName.BackColor = Color.FromArgb(133, 205, 253);
+            Label_CampaignName.Font = new Font("Segoe UI", 40F, FontStyle.Bold, GraphicsUnit.Pixel);
+            Label_CampaignName.ForeColor = Color.White;
+            Label_CampaignName.Location = new Point(590, 314);
+            Label_CampaignName.Name = "Label_CampaignName";
+            Label_CampaignName.Size = new Size(550, 56);
+            Label_CampaignName.TabIndex = 9;
+            Label_CampaignName.Text = "HỘI TRƯỞNG HỘI HỌC SINH";
+            // 
+            // guna2HtmlLabel3
+            // 
+            guna2HtmlLabel3.BackColor = Color.White;
+            guna2HtmlLabel3.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Pixel);
+            guna2HtmlLabel3.ForeColor = Color.Black;
+            guna2HtmlLabel3.Location = new Point(73, 727);
+            guna2HtmlLabel3.Name = "guna2HtmlLabel3";
+            guna2HtmlLabel3.Size = new Size(318, 50);
+            guna2HtmlLabel3.TabIndex = 10;
+            guna2HtmlLabel3.Text = "Campaign promise";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ControlLightLight;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 21F, FontStyle.Regular, GraphicsUnit.Pixel);
+            textBox1.ForeColor = Color.MidnightBlue;
+            textBox1.Location = new Point(73, 792);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(387, 69);
+            textBox1.TabIndex = 14;
+            textBox1.Text = "Tôi là Hiệp và tôi thích đi Vũng Tàu.\nThank you!";
+            // 
+            // guna2HtmlLabel4
+            // 
+            guna2HtmlLabel4.BackColor = Color.FromArgb(133, 205, 253);
+            guna2HtmlLabel4.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Pixel);
+            guna2HtmlLabel4.ForeColor = Color.White;
+            guna2HtmlLabel4.Location = new Point(389, 477);
+            guna2HtmlLabel4.Name = "guna2HtmlLabel4";
+            guna2HtmlLabel4.Size = new Size(48, 37);
+            guna2HtmlLabel4.TabIndex = 15;
+            guna2HtmlLabel4.Text = "Tên:";
+            // 
+            // guna2HtmlLabel5
+            // 
+            guna2HtmlLabel5.BackColor = Color.FromArgb(133, 205, 253);
+            guna2HtmlLabel5.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Pixel);
+            guna2HtmlLabel5.ForeColor = Color.White;
+            guna2HtmlLabel5.Location = new Point(389, 534);
+            guna2HtmlLabel5.Name = "guna2HtmlLabel5";
+            guna2HtmlLabel5.Size = new Size(50, 37);
+            guna2HtmlLabel5.TabIndex = 16;
+            guna2HtmlLabel5.Text = "Lớp:";
+            // 
+            // guna2HtmlLabel6
+            // 
+            guna2HtmlLabel6.BackColor = Color.FromArgb(133, 205, 253);
+            guna2HtmlLabel6.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Pixel);
+            guna2HtmlLabel6.ForeColor = Color.White;
+            guna2HtmlLabel6.Location = new Point(389, 590);
+            guna2HtmlLabel6.Name = "guna2HtmlLabel6";
+            guna2HtmlLabel6.Size = new Size(56, 37);
+            guna2HtmlLabel6.TabIndex = 17;
+            guna2HtmlLabel6.Text = "Tuổi:";
+            // 
+            // Label_Slogan
+            // 
+            Label_Slogan.BackColor = Color.FromArgb(133, 205, 253);
+            Label_Slogan.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Pixel);
+            Label_Slogan.ForeColor = Color.White;
+            Label_Slogan.Location = new Point(389, 642);
+            Label_Slogan.Name = "Label_Slogan";
+            Label_Slogan.Size = new Size(430, 37);
+            Label_Slogan.TabIndex = 18;
+            Label_Slogan.Text = "“ Vote for me and i will be your voice ”";
+            // 
+            // Label_Name_Cadidate
+            // 
+            Label_Name_Cadidate.BackColor = Color.FromArgb(133, 205, 253);
+            Label_Name_Cadidate.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Pixel);
+            Label_Name_Cadidate.ForeColor = Color.White;
+            Label_Name_Cadidate.Location = new Point(451, 477);
+            Label_Name_Cadidate.Name = "Label_Name_Cadidate";
+            Label_Name_Cadidate.Size = new Size(200, 37);
+            Label_Name_Cadidate.TabIndex = 19;
+            Label_Name_Cadidate.Text = "Đào Võ Hữu Hiệp";
+            // 
+            // Label_Class
+            // 
+            Label_Class.BackColor = Color.FromArgb(133, 205, 253);
+            Label_Class.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Pixel);
+            Label_Class.ForeColor = Color.White;
+            Label_Class.Location = new Point(451, 534);
+            Label_Class.Name = "Label_Class";
+            Label_Class.Size = new Size(97, 37);
+            Label_Class.TabIndex = 20;
+            Label_Class.Text = "ATT2021";
+            // 
+            // guna2HtmlLabel9
+            // 
+            guna2HtmlLabel9.AccessibleDescription = "Label_age";
+            guna2HtmlLabel9.BackColor = Color.FromArgb(133, 205, 253);
+            guna2HtmlLabel9.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Pixel);
+            guna2HtmlLabel9.ForeColor = Color.White;
+            guna2HtmlLabel9.Location = new Point(451, 590);
+            guna2HtmlLabel9.Name = "guna2HtmlLabel9";
+            guna2HtmlLabel9.Size = new Size(29, 37);
+            guna2HtmlLabel9.TabIndex = 21;
+            guna2HtmlLabel9.Text = "20";
+            // 
+            // Info_Candidate
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoScroll = true;
+            ClientSize = new Size(1422, 903);
+            Controls.Add(guna2HtmlLabel9);
+            Controls.Add(Label_Class);
+            Controls.Add(Label_Name_Cadidate);
+            Controls.Add(Label_Slogan);
+            Controls.Add(guna2HtmlLabel6);
+            Controls.Add(guna2HtmlLabel5);
+            Controls.Add(guna2HtmlLabel4);
+            Controls.Add(textBox1);
+            Controls.Add(guna2HtmlLabel3);
+            Controls.Add(Label_CampaignName);
+            Controls.Add(guna2HtmlLabel2);
+            Controls.Add(guna2HtmlLabel1);
+            Controls.Add(Label_name);
+            Controls.Add(guna2CirclePictureBox2);
+            Controls.Add(guna2CircleButton1);
+            Controls.Add(guna2CirclePictureBox1);
+            Controls.Add(guna2PictureBox3);
+            Controls.Add(guna2PictureBox2);
+            Controls.Add(guna2PictureBox1);
+            Name = "Info_Candidate";
+            Text = "Info_Candidate";
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox2;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox3;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton1;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel Label_name;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel Label_CampaignName;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
+        private TextBox textBox1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel4;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel5;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel6;
+        private Guna.UI2.WinForms.Guna2HtmlLabel Label_Slogan;
+        private Guna.UI2.WinForms.Guna2HtmlLabel Label_Name_Cadidate;
+        private Guna.UI2.WinForms.Guna2HtmlLabel Label_Class;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel9;
+    }
+}
---
+++ DOANMONHOC/DOANMONHOC/Info_Candidate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DOANMONHOC
+{
+    public partial class Info_Candidate : Form
+    {
+        public Info_Candidate()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
